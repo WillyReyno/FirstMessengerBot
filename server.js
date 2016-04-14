6206e52e1d1397b402ed16b3b2cebac07172e2b0
WillyReyno <reyno.willy@gmail.com>
2016-04-14 15:10:38 +0200

    change quotes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app     = express();
 var port    =   process.env.PORT || 8080;
 
 app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === "lets_verify_my_token") {
+  if (req.query['hub.verify_token'] === 'lets_verify_my_token') {
     res.send(req.query['hub.challenge']);
   } else {
     res.send('Error, wrong validation token');    
